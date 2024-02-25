3526cdf4650a5437ce7e698654a11b146140ca55
Abhishek Tripathy <yuabhishek14@gmail.com>
2024-02-25 16:59:47 +0530

    modified both files

--- mediawiki.tf
+++ mediawiki.tf
@@ -22,7 +22,10 @@ resource "aws_instance" "mediawiki" {
 
   provisioner "remote-exec" {
      inline = [
-      "sudo su -",
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install -y ansible",
       "mkdir -p /opt"
         ]
   }
